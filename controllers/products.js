f1be090f328e69a73328674ae03a1a4806d3c9da
khanhngo.pix@gmail.com <khanhngo.pix@gmail.com>
2022-12-26 23:05:10 +0700

    u

--- controllers/products.js
+++ controllers/products.js
@@ -31,67 +31,67 @@ export const getItem = (req, res, next) => {
 }
 
 export const getListItem = async (req, res, next) => {
-    for(let i = 1; i < 20; i++) {    
-        collection.insertOne(  {
-            image: 'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/10-255x348.jpg',
-            subImage: [
-                'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/15-255x348.jpg',
-                'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/3-255x348.jpg',
-                'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/2-255x348.jpg',
-                'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/1-255x348.jpg'
-            ],
-            name: `casual shirt ${i}`,
-            category: 'men',
-            color_group: [
-                {
-                    hex: '#ff0000de',
-                    active: true,
-                    name: 'red'
-                },
-                {
-                    hex: '#ff9900de',
-                    active: false,
-                    name: 'cam'
-                },
-                {
-                    hex: '#33ff00de',
-                    active: false,
-                    name: 'xanh la'
-                },
-            ],
-            size_group: [
-                {
-                    name: 'S',
-                    active: true
-                },
-                {
-                    name: 'M',
-                    active: false
-                },
-                {
-                    name: 'XL',
-                    active: false
-                }
-            ],
-            color: '#ff0000de',
-            size: 'S',
-            slug: 'casual-shirt',
-            type: 'special',
-            like: 1,
-            price: 140000,
-            discount: 0,
-            rating: 4,
-            view: 12,
-            share: 0,
-            quantity: 1,
-            comment: [
-                {
-                    userId: '',
-                    desc: ''
-                }
-            ]
-        })
-    }
+    // for(let i = 1; i < 20; i++) {    
+    //     collection.insertOne(  {
+    //         image: 'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/10-255x348.jpg',
+    //         subImage: [
+    //             'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/15-255x348.jpg',
+    //             'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/3-255x348.jpg',
+    //             'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/2-255x348.jpg',
+    //             'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/1-255x348.jpg'
+    //         ],
+    //         name: `casual shirt ${i}`,
+    //         category: 'men',
+    //         color_group: [
+    //             {
+    //                 hex: '#ff0000de',
+    //                 active: true,
+    //                 name: 'red'
+    //             },
+    //             {
+    //                 hex: '#ff9900de',
+    //                 active: false,
+    //                 name: 'cam'
+    //             },
+    //             {
+    //                 hex: '#33ff00de',
+    //                 active: false,
+    //                 name: 'xanh la'
+    //             },
+    //         ],
+    //         size_group: [
+    //             {
+    //                 name: 'S',
+    //                 active: true
+    //             },
+    //             {
+    //                 name: 'M',
+    //                 active: false
+    //             },
+    //             {
+    //                 name: 'XL',
+    //                 active: false
+    //             }
+    //         ],
+    //         color: '#ff0000de',
+    //         size: 'S',
+    //         slug: 'casual-shirt',
+    //         type: 'special',
+    //         like: 1,
+    //         price: 140000,
+    //         discount: 0,
+    //         rating: 4,
+    //         review: 12,
+    //         share: 0,
+    //         quantity: 1,
+    //         comment: [
+    //             {
+    //                 userId: '',
+    //                 desc: ''
+    //             }
+    //         ]
+    //     })
+    // }
     try {
         const limit = Number(req.query.limit) || 10;
         const page = Number(req.query.page) || 1;
