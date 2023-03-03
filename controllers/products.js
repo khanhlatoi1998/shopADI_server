a3ef84df8ba01cb1a6d4fe4b24442e9588aea8d7
khanhngo.pix@gmail.com <khanhngo.pix@gmail.com>
2023-03-03 14:27:15 +0700

    u

--- controllers/products.js
+++ controllers/products.js
@@ -31,66 +31,56 @@ export const getItem = (req, res, next) => {
 }
 
 export const getListItem = async (req, res, next) => {
-    // for(let i = 1; i < 20; i++) {    
-    //     collection.insertOne(  {
-    //         image: 'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/10-255x348.jpg',
-    //         listImage: [
-    //             'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/15-255x348.jpg',
-    //             'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/3-255x348.jpg',
-    //             'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/2-255x348.jpg',
-    //             'https://opencart.mahardhi.com/MT01/adi/image/cache/catalog/product/1-255x348.jpg'
-    //         ],
-    //         name: `casual shirt ${i}`,
-    //         category: 'men',
-    //         color_group: [
-    //             {
-    //                 hex: '#ff0000de',
-    //                 active: true,
-    //                 name: 'red'
-    //             },
-    //             {
-    //                 hex: '#ff9900de',
-    //                 active: false,
-    //                 name: 'cam'
-    //             },
-    //             {
-    //                 hex: '#33ff00de',
-    //                 active: false,
-    //                 name: 'xanh la'
-    //             },
-    //         ],
-    //         size_group: [
-    //             {
-    //                 name: 'S',
-    //                 active: true
-    //             },
-    //             {
-    //                 name: 'M',
-    //                 active: false
-    //             },
-    //             {
-    //                 name: 'XL',
-    //                 active: false
-    //             }
-    //         ],
-    //         color: '#ff0000de',
-    //         size: 'S',
-    //         slug: 'casual-shirt',
-    //         type: 'special',
-    //         like: 1,
-    //         price: 140000,
-    //         oldPrice: 0,
-    //         rating: 4,
-    //         review: 12,
-    //         share: 0,
-    //         quantity: 1,
-    //         comment: [
-    //             {
-    //                 userId: '',
-    //                 desc: ''
-    //             }
-    //         ]
-    //     })
+    // for(let i = 0; i <= 20; i++) {    
+    //     collection.insertOne(
+    //         {
+    //             "image": "https://firebasestorage.googleapis.com/v0/b/shop-adi.appspot.com/o/Products-Image%2F5458eb16-7192-a195-9722-de1d6df417e0?alt=media&token=aacb2975-dba7-4c0b-8d48-3cf408fdd180",
+    //             "subImage": [
+    //                 "https://firebasestorage.googleapis.com/v0/b/shop-adi.appspot.com/o/Products-Image%2F5458eb16-7192-a195-9722-de1d6df417e0?alt=media&token=aacb2975-dba7-4c0b-8d48-3cf408fdd180",
+    //                 "https://firebasestorage.googleapis.com/v0/b/shop-adi.appspot.com/o/Products-Image%2F666530ad-1601-73dd-da8f-b1d580a4f87c?alt=media&token=29bcf9b4-5d32-4ff9-b6ca-4372ff4411e1",
+    //                 "https://firebasestorage.googleapis.com/v0/b/shop-adi.appspot.com/o/Products-Image%2F7849f5da-d382-0d15-8036-36cc1f5fb8b0?alt=media&token=187be663-c448-4b8c-9e35-6d05f464e74e",
+    //                 "https://firebasestorage.googleapis.com/v0/b/shop-adi.appspot.com/o/Products-Image%2F588194b0-d2df-a220-f956-9e5ac6703fcf?alt=media&token=fde5719f-5282-4c9e-a584-1c5627279d8c"
+    //             ],
+    //             "name": "name",
+    //             "category": "men",
+    //             "color_group": [
+    //                 {
+    //                     "hex": "#ff0000",
+    //                     "name": "#ff0000",
+    //                     "active": true
+    //                 },
+    //                 {
+    //                     "hex": "blue",
+    //                     "name": "blue",
+    //                     "active": false
+    //                 },
+    //                 {
+    //                     "hex": "yellow",
+    //                     "name": "yellow",
+    //                     "active": false
+    //                 }
+    //             ],
+    //             "size_group": [
+    //                 "S",
+    //                 "M",
+    //                 "L",
+    //                 "X"
+    //             ],
+    //             "color": "#4f1c1c",
+    //             "size": "S",
+    //             "slug": "slug-6538378",
+    //             "price": 6000 + i*10,
+    //             "oldPrice": 7000 +i*20,
+    //             "quantity": 1,
+    //             "rating": "",
+    //             "type": "",
+    //             "like": 0,
+    //             "view": 0,
+    //             "share": 0,
+    //             "comment": [],
+    //             "description": ""
+    //         }
+    //     )
     // }
     try {
         const limit = Number(req.query.limit) || 10;
